323c3f31bc3db24db7eaca94253304edeb325ced
csco379 <csco379@aucklanduni.ac.nz>
2021-10-13 15:06:11 +1300

    removing variables

--- SolveLP.py
+++ SolveLP.py
@@ -161,10 +161,10 @@ def solveLP(Weekday):
     # Saving the route indices for use in visualising
     if Weekday == True:
         np.savetxt("RouteVector_Weekday.csv", routeNumbers, delimiter=',')
-        np.savetxt("RouteStores_Weekday.csv", routeStores, delimiter=',')
+ 
     else:
         np.savetxt("RouteVector_Weekend.csv", routeNumbers, delimiter=',')
-        np.savetxt("RouteStores_Weekend.csv", routeStores, delimiter=',')
+
 
 
     allvisited = "False"
